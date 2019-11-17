98f1c2630d0eaaba1f18770b464a768667023d81
Paul Mahon <paulmahon@me.com>
2019-11-17 17:28:40 -0500

    updates

--- hash_message.py
+++ hash_message.py
@@ -1,24 +1,36 @@
+# This file signs a file with the owner's private key and
+# verifies the signature with the owners public key
 import rsa
 
-# open public key file and load public key
-key_file = open('privkey.key', 'rb')
-key_data = key_file.read()
-key_file.close()
-privkey = rsa.PrivateKey.load_pkcs1(key_data)
 
-file_d = open('message', 'rb')
-message = file_d.read()
+# Open key file and return key data
+def file_open(file):
+    key_file = open(file, 'rb')
+    key_data = key_file.read()
+    key_file.close()
+    return key_data
 
+
+# Open private key file and load in key
+privkey = rsa.PrivateKey.load_pkcs1(file_open('privkey.key'))
+
+# Open the secret message file and return data to variable
+message = file_open('message')
+
+# Sign the message with the owners private key
 signature = rsa.sign(message, privkey, 'SHA-256')
 
 print(signature)
 
-key_file = open('publickey.key', 'rb')
-key_data = key_file.read()
-key_file.close()
-pubkey = rsa.PublicKey.load_pkcs1(key_data)
-sigver = rsa.verify(message, signature, pubkey)
+# change the message to show verification
 
-print(sigver)
+# Open public key file and load in key
+pubkey = rsa.PublicKey.load_pkcs1(file_open('publickey.key'))
 
+# Verify the signature to show if successful or failed
+try:
+    rsa.verify(message, signature, pubkey)
+    print("Signature successfully verified")
 
+except:
+    print("Warning!!!! Signature could not be verified")
