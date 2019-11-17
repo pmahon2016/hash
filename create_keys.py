745d8a7dbd1f587056989bf35b86042f7c39930f
Paul Mahon <paulmahon@me.com>
2019-11-17 18:55:45 -0500

    key updates

--- create_keys.py
+++ create_keys.py
@@ -1,23 +1,18 @@
 import rsa
-from cryptography.fernet import Fernet
 
-# create the symmetric key
-key = Fernet.generate_key()
 
-# write the symmetric key to a file
-k = open('symmetric.key','wb')
-k.write(key)
-k.close()
+def create_file(file):
+    key_file = open(file, 'wb')
+    return key_file
+
 
 # create the pub & private keys
-(pubkey,privkey)=rsa.newkeys(2048)
+(pubkey, privkey) = rsa.newkeys(2048)
 
 # write the public key to a file
-pukey = open('publickey.key','wb')
-pukey.write(pubkey.save_pkcs1('PEM'))
-pukey.close()
+create_file('publickey.key').write(pubkey.save_pkcs1('PEM'))
 
 # write the private key to a file
-prkey = open('privkey.key','wb')
-prkey.write(privkey.save_pkcs1('PEM'))
-prkey.close()
+create_file('privatekey.key').write(privkey.save_pkcs1('PEM'))
+
+
